14f53fff9ba08aa5d49c88a564d605ba6de3b382
qiyao <309103737@qq.com>
2014-06-16 21:20:12 +0800

    products add

---
+++ bd-products/src/main/java/com/changhong/bd/products/entity/ProductEntity.java
@@ -0,0 +1,11 @@
+package com.changhong.bd.products.entity;
+/**
+ * @author QiYao  yao.qi@changhong.com
+ * @date 2014年6月15日
+ * @version 1.0
+ * @description : 商品信息
+ */
+
+public class ProductEntity {
+
+}
