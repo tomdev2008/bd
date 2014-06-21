ff3e111eb090924feb7e8f0297161b99c45152f0
qiyao <309103737@qq.com>
2014-06-21 11:03:44 +0800

    mv

--- bd-lotaste/src/main/java/com/changhong/bd/lotaste/web/IndexController.java
+++ bd-lotaste/src/main/java/com/changhong/bd/lotaste/web/IndexController.java
@@ -1,4 +1,4 @@
-package com.changhong.bd.loteste.web;
+package com.changhong.bd.lotaste.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
