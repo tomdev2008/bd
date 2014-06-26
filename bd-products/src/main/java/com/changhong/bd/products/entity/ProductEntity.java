186707a7ec482afafe668d2f6680b23325b733e8
qiyao <309103737@qq.com>
2014-06-26 09:37:00 +0800

    add product

--- bd-products/src/main/java/com/changhong/bd/products/entity/ProductEntity.java
+++ bd-products/src/main/java/com/changhong/bd/products/entity/ProductEntity.java
@@ -1,11 +1,24 @@
 package com.changhong.bd.products.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.changhong.bd.core.entity.abs.AbsTxNameEntity;
+
 /**
  * @author QiYao  yao.qi@changhong.com
  * @date 2014年6月15日
  * @version 1.0
- * @description : 商品信息
+ * @description : 商品基本库信息
  */
+@Entity
+@Table(name="products_product")
+public class ProductEntity extends AbsTxNameEntity{
 
-public class ProductEntity {
+	private static final long serialVersionUID = -8761936127669640029L;
 
+	//计量单位
+	@Column(name="measureunit")
+	private String measureUnit = "斤";
 }
