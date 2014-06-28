dcd3885be03e7830ba4a39073eb27efd7a21ade6
qiyao <309103737@qq.com>
2014-06-28 11:44:42 +0800

    地址实体

---
+++ bd-crm/src/main/java/com/changhong/bd/crm/entity/CustomerLocationEntity.java
@@ -0,0 +1,10 @@
+package com.changhong.bd.crm.entity;
+/**
+ * @author QiYao  yao.qi@changhong.com
+ * @date 2014年6月28日
+ * @version 1.0
+ * @description : 客户地址实体
+ */
+public class CustomerLocationEntity {
+
+}
